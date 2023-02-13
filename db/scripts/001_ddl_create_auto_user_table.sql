74ad7d294499fbce29dca9b05c775c221fa9194e
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-02-13 12:48:42 +0300

    2. Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/001_ddl_create_auto_user_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE auto_user
+(
+    id       serial primary key,
+    login    varchar unique not null,
+    password varchar        not null
+);
---
+++ db/scripts/002_ddl_create_auto_post_table.sql
@@ -0,0 +1,8 @@
+create table auto_post
+(
+    id            serial primary key,
+    description   varchar not null,
+    created       timestamp not null,
+    auto_user_id  int references auto_user(id)
+
+);
