8b5890a5eccad08991b44d66820a82145cf92ae5
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-03-13 12:55:51 +0300

    2. ManyToMany [#331986]

---
+++ db/scripts/005_ddl_create_participates_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE participates (
+   id serial PRIMARY KEY,
+   post_id int not null REFERENCES auto_post(id),
+   user_id int not null REFERENCES auto_user(id)
+);
--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -30,4 +30,12 @@ public class Post {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "auto_post_id")
     private List<PriceHistory> priceHistoryList;
+
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = {@JoinColumn(name = "post_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")}
+    )
+    private List<User> participates;
 }
